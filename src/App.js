e699f9529ae354a1bcc37484d3971da2926bbf61
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-09-24 14:48:57 +0600

    Semantic Ui setup - yarn add semantic-ui-react semantic-ui-css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage'
-const App = () => <div>
-  <Route path="/" exact component={HomePage} />
-  <Route path="/login" exact component={LoginPage} />
-</div>;
+
+const App = () => (
+  <div className="ui container">
+    <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
+  </div>
+);
 
 export default App;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+
 const HomePage = () => (
   <div>
     <h1>Home Page</h1>
