913d2e194e3cbf888eb43b7a2e1ac818a5434099
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-09-24 14:32:57 +0600

    React router and React-router-dom setup - yarn add react-router - yarn add react-router-dom - created homepage and login page - Created login and homepage routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-
-const App = () => <div>Hello World</div>;
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage'
+const App = () => <div>
+  <Route path="/" exact component={HomePage} />
+  <Route path="/login" exact component={LoginPage} />
+</div>;
 
 export default App;
