a422fee8ed9394aadf1fc79ed9d6063506c7288a
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-10-08 00:52:21 +0600

    Errors messages from server is displayed on client side - Message header of semantic-ui is used - loader animation from semantic-ui is provided

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Message } from 'semantic-ui-react';
 import Validator from 'validator';
 import PropTypes from 'prop-types';
 import InlineError from "../messages/InlineError";
@@ -22,7 +22,9 @@ class LoginForm extends React.Component {
    const errors = this.validate(this.state.data);
    this.setState({ errors });
    if(Object.keys(errors).length === 0){
-     this.props.submit(this.state.data);
+     this.setState({loading: true});
+     this.props.submit(this.state.data)
+      .catch(err => this.setState({errors: err.response.data.errors, loading: false}));
    }
  }
 
@@ -33,9 +35,13 @@ class LoginForm extends React.Component {
    return errors;
  }
   render(){
-    const { data, errors } = this.state;
+    const { data, errors, loading } = this.state;
     return(
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} loading={loading}>
+        { errors.global && <Message negative>
+          <Message.Header> Something went wrong </Message.Header>
+          <p>{errors.global}</p>
+        </Message>}
       <Form.Field error={!!errors.email}>
         <label htmlFor="email">Email</label>
         <input
