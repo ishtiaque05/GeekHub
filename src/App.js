0602f4a9c62dc86aeccef3628c649a234d642229
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-10-16 22:11:41 +0600

    Front end signup form and api call implemented - TODO:: Backend server request needs to be handled - signup form email and password validation added - loading and signup errors notification handled

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import  PropTypes from 'prop-types';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 import DashboardPage from './components/pages/DashboardPage';
+import SignupPage from './components/pages/SignupPage';
 import UserRoute from './components/routes/UserRoute';
 import GuestRoute from './components/routes/GuestRoute';
 
@@ -11,6 +12,7 @@ const App = ({ location }) => (
   <div className="ui container">
     <Route location={location} path="/" exact component={HomePage} />
     <GuestRoute location={location} path="/login" exact component={LoginPage} />
+    <GuestRoute location={location} path="/signup" exact component={SignupPage} />
     <UserRoute location={location} path="/dashboard" exact component={DashboardPage} />
   </div>
 );
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -7,7 +7,11 @@ import * as actions from '../../actions/auth';
 const HomePage = ({ isAuthenticated, logout }) => (
   <div>
     <h1>Home Page</h1>
-    { isAuthenticated ? <button onClick={() => logout() }>Logout</button> : <Link to="/login">Login</Link> }
+    { isAuthenticated ? (
+      <button onClick={() => logout() }>Logout</button>
+    ) : (
+      <div><Link to="/login">Login</Link> or <Link to="/signup">Sign Up</Link></div>
+  )}
   </div>
 );
 
