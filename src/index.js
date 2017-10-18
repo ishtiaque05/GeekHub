35520a67053939b61978d42fbee3a465ae8a1076
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-10-19 00:49:42 +0600

    Front-end Confirm email feature implemented - when user click confirmation link email is confirmed - loading success for confirmation email implemented - maintain confirmed state by decoding jwt token - added jwt-decode ( yarn add jwt-decode)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.min.css';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import decode from 'jwt-decode';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -14,7 +15,12 @@ import { userLoggedIn } from './actions/auth';
 const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
 
 if(localStorage.bookwormJWT){
-  const user = { token: localStorage.bookwormJWT };
+  const payload = decode(localStorage.bookwormJWT);
+  const user = {
+    token: localStorage.bookwormJWT,
+    email: payload.email,
+    confirmed: payload.confirmed
+   };
   store.dispatch(userLoggedIn(user));
 }
 
