aea0ee72e27436ad82a12f025332fbbfd41c1ecf
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-10-20 17:54:20 +0600

    User can reset password - click forgot password to initiate password change events - user click reset password link and reset password using reset password form - client side validation for password confirmatio match implemented - Client side validation error message display handled - client request server with new password and jwt token

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import LoginForm from "../forms/LoginForm";
 import { login } from '../../actions/auth';
 
@@ -15,6 +16,7 @@ class LoginPage extends React.Component {
       <div>
         <h1>Login page</h1>
         <LoginForm submit={this.submit}/>
+        <Link to='/forgot_password'>Forgot password</Link>
       </div>
     );
   }
---
+++ src/components/pages/ResetPasswordPage.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Message } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { validateToken, resetPassword } from '../../actions/auth';
+import ResetPasswordForm from '../forms/ResetPasswordForm';
+
+class ResetPasswordPage extends React.Component {
+  state = {
+    loading: true,
+    success: false
+  }
+
+  componentDidMount(){
+    this.props.validateToken(this.props.match.params.token)
+     .then(() =>  this.setState({loading: false, success: true}))
+     .catch(() => this.setState({loading: false, success: false }));
+  }
+
+  submit = data =>
+   this.props
+   .resetPassword(data)
+   .then(() => this.props.history.push('/login'));
+
+  render() {
+    const { loading, success } = this.state;
+    const token = this.props.match.params.token;
+    return(
+      <div>
+        { loading && <Message>Loading</Message> }
+        { !loading && success &&
+          <ResetPasswordForm submit={this.submit} token={ token } /> }
+        { !loading && !success && <Message>Invalid Token</Message> }
+      </div>
+    );
+  }
+}
+
+ResetPasswordPage.propTypes = {
+  validateToken: PropTypes.func.isRequired,
+  resetPassword: PropTypes.func.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      token: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired
+}
+
+export default connect(null, { validateToken, resetPassword })(ResetPasswordPage);
