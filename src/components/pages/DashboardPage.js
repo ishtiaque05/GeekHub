975371c8a0e7110f1a9c0f2686dc6a9d7887587f
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-10-20 23:41:07 +0600

    Added Nav bar for authenticated user & add new book icon - created a top navigation for authenticated user - added gravatar-url ( yarn add gravatar-url) - added reselect ( yarn add reselect) - added logout in drop down menu in top navigation bar

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -2,21 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ConfirmEmailMessage from '../messages/ConfirmEmailMessage';
+import { allBooksSelector } from '../../reducers/books';
+import AddBookCtA from '../ctas/AddBookCtA';
 
-const DashboardPage = ({ isConfirmed }) => (
+const DashboardPage = ({ isConfirmed, books }) => (
   <div>
     { !isConfirmed && <ConfirmEmailMessage/> }
+    { books.length === 0 && <AddBookCtA /> }
   </div>
 );
 
 DashboardPage.propTypes = {
-  isConfirmed: PropTypes.bool.isRequired
+  isConfirmed: PropTypes.bool.isRequired,
+  books: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired
+  }).isRequired).isRequired
 };
 
 function mapStateToProps(state){
   return {
-    isConfirmed: !!state.user.confirmed
-  }
+    isConfirmed: !!state.user.confirmed,
+    books: allBooksSelector(state)
+  };
 }
 
 export default connect(mapStateToProps)(DashboardPage);
---
+++ src/components/pages/NewBookPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class NewBookPage extends React.Component {
+  state = {
+    book: null
+  }
+
+  render(){
+    return (
+      <div />
+    );
+  }
+}
+
+export default NewBookPage;
