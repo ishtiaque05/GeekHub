14c346c528c5b7a098ead5450f37b2e3e9acae68
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-10-28 01:24:13 +0600

    Sent post create request for books - check if books are added if so no plus button is displayed - if books are present nav bar shows add more books label - fetch page number to display it

--- src/components/navigation/TopNavigation.js
+++ src/components/navigation/TopNavigation.js
@@ -5,10 +5,12 @@ import gravatarUrl from 'gravatar-url';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/auth';
+import { allBooksSelector } from "../../reducers/books";
 
-const TopNavigation = ({ user, logout }) => (
+const TopNavigation = ({ user, logout, hasBooks }) => (
   <Menu secondary pointing>
     <Menu.Item as={Link} to='/dashboard'> Dashboard </Menu.Item>
+    { hasBooks &&   <Menu.Item as={Link} to='/books/new'> Add new book </Menu.Item>}
     <Menu.Menu position="right">
       <Dropdown trigger={<Image avatar src={gravatarUrl(user.email)} />}>
         <Dropdown.Menu>
@@ -23,12 +25,14 @@ TopNavigation.propTypes = {
   user: PropTypes.shape({
     email: PropTypes.string.isRequired
   }).isRequired,
+  hasBooks: PropTypes.bool.isRequired,
   logout: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state){
   return {
-    user: state.user
+    user: state.user,
+    hasBooks: allBooksSelector(state).length > 0
   }
 }
 
